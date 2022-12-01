fb7e41c7ceb3481b69fc791f1293fa895b2ba9cf
ALVAROPING1 <alvaroping1@gmail.com>
2022-12-01 18:40:14 +0100

    simplified solution for day 1

--- 2022/Day1/part1.py
+++ 2022/Day1/part1.py
@@ -1,13 +1,8 @@
 def count(_input: str) -> int:
 	maximum = 0
-	current = 0
 	with open(_input, "r", encoding="UTF-8") as file:
-		for element in file.readlines():
-			if element == "\n":
-				maximum = max(maximum, current)
-				current = 0
-			else:
-				current += int(element)
+		for element in file.read().split("\n\n"):
+			maximum = max(maximum, sum(int(x) for x in element.split("\n")))
 		return maximum
 
-print(count("./Day1/input.txt"))
+print(count("./2022/Day1/input.txt"))
--- 2022/Day1/part2.py
+++ 2022/Day1/part2.py
@@ -1,17 +1,10 @@
 def count(_input: str) -> int:
 	maximum = [0,0,0]
-	current = 0
 	with open(_input, "r", encoding="UTF-8") as file:
-		for element in file.readlines():
-			if element == "\n":
-				if current > maximum[0]:
-					if current > maximum[1]: 
-						if current > maximum[2]: maximum = maximum[1:] + [current]
-						else: maximum[0:2] = [maximum[1], current]
-					else: maximum[0] = current
-				current = 0
-			else:
-				current += int(element)
+		for element in file.read().split("\n\n"):
+			current = sum(int(x) for x in element.split("\n"))
+			for i in range(2, -1, -1):
+				maximum[i], current = max(current, maximum[i]), min(current, maximum[i]) 
 	return sum(maximum)
 
-print(count("./Day1/input.txt"))
+print(count("./2022/Day1/input.txt"))
