6cd5d9f3e82f9128edd00daa8c978bae36c68a7d
ALVAROPING1 <alvaroping1@gmail.com>
2022-12-02 17:12:14 +0100

    improved day 1 solution

--- 2022/Day1/part1.py
+++ 2022/Day1/part1.py
@@ -1,8 +1,5 @@
 def count(_input: str) -> int:
-	maximum = 0
 	with open(_input, "r", encoding="UTF-8") as file:
-		for element in file.read().split("\n\n"):
-			maximum = max(maximum, sum(int(x) for x in element.split("\n")))
-		return maximum
+		return max(map(sum, (map(int, x.split("\n")) for x in file.read().split("\n\n"))))
 
 print(count("./2022/Day1/input.txt"))
--- 2022/Day1/part2.py
+++ 2022/Day1/part2.py
@@ -1,10 +1,7 @@
-def count(_input: str) -> int:
-	maximum = [0,0,0]
-	with open(_input, "r", encoding="UTF-8") as file:
-		for element in file.read().split("\n\n"):
-			current = sum(int(x) for x in element.split("\n"))
-			for i in range(2, -1, -1):
-				maximum[i], current = max(current, maximum[i]), min(current, maximum[i]) 
-	return sum(maximum)
+from heapq import nlargest
 
+def count(_input: str) -> int:
+	with open(_input, "r", encoding="UTF-8") as file:
+		return sum(nlargest(3, map(sum, (map(int, x.split("\n")) for x in file.read().split("\n\n")))))
+ 
 print(count("./2022/Day1/input.txt"))
