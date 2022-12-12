d3b69ef79aa78141b8b1e334554cc159aa53da49
ALVAROPING1 <alvaroping1@gmail.com>
2022-12-12 13:50:19 +0100

    simplified day 9 solution

--- 2022/Day9/solution.py
+++ 2022/Day9/solution.py
@@ -3,49 +3,46 @@ def part1(_input: str) -> int:
 	letter2direction = {"L": -1, "R": 1, "U": 1, "D": -1}
 	with open(_input, "r", encoding="UTF-8") as file:
 		moves = tuple((letter2axis[x[0]], letter2direction[x[0]], int(x [2:])) for x in file.readlines())
-	maxX, _, minX, minY = calculate_grid_size(moves)
+	maxX, maxY, minX, minY = calculate_grid_size(moves)
 	current_head_position = [-minX, -minY]
 	tail = RopeSegment(*current_head_position)
-	line_length = maxX - minX
-	visited_positions = matrix_positions2bit_field((current_head_position,), line_length)
+	visited_positions = [[False for _ in range(maxY-minY)] for _ in range(maxX - minX)]
+	visited_positions[current_head_position[0]][current_head_position[1]] = True
 	for move in moves:
 		current_head_position[move[0]] += move[1] * move[2]
 		new_positions = tail.move_towards_point(*current_head_position)
-		new_bit_field = matrix_positions2bit_field(new_positions, line_length)
-		visited_positions = visited_positions | new_bit_field
-	return visited_positions.bit_count()
+		for new_position in new_positions:
+			visited_positions[new_position[0]][new_position[1]] = True
+	return sum(sum(line) for line in visited_positions)
 
 def part2(_input: str) -> int:
 	letter2axis = {"L": 0, "R": 0, "U": 1, "D": 1}
 	letter2direction = {"L": -1, "R": 1, "U": 1, "D": -1}
 	with open(_input, "r", encoding="UTF-8") as file:
 		moves = tuple((letter2axis[x[0]], letter2direction[x[0]], int(x [2:])) for x in file.readlines())
-	maxX, _, minX, minY = calculate_grid_size(moves)
+	maxX, maxY, minX, minY = calculate_grid_size(moves)
 	current_head_position = [-minX, -minY]
-	mid_sections = tuple(RopeSegment(*current_head_position) for _ in range(8))
-	tail = RopeSegment(*current_head_position)
-	line_length = maxX - minX
-	visited_positions = matrix_positions2bit_field((current_head_position,), line_length)
+	tails = tuple(RopeSegment(*current_head_position) for _ in range(9))
+	visited_positions = [[False for _ in range(maxY-minY)] for _ in range(maxX - minX)]
+	visited_positions[current_head_position[0]][current_head_position[1]] = True
 	for move in moves:
 		for _ in range(move[2]):
 			current_head_position[move[0]] += move[1]
 			previous_rope_position = current_head_position
-			for rope in mid_sections:
-				new_mid_points = rope.move_towards_point(*previous_rope_position)
-				if len(new_mid_points) == 0:
+			for rope in tails:
+				new_positions = rope.move_towards_point(*previous_rope_position)
+				if len(new_positions) == 0:
 					break
 				previous_rope_position = (rope.positionX, rope.positionY)
-			if len(new_mid_points) != 0:
-				new_positions = tail.move_towards_point(mid_sections[-1].positionX, mid_sections[-1].positionY)
-				new_bit_field = matrix_positions2bit_field(new_positions, line_length)
-				visited_positions = visited_positions | new_bit_field
-	return visited_positions.bit_count()
+			for new_position in new_positions:
+				visited_positions[new_position[0]][new_position[1]] = True
+	return sum(sum(line) for line in visited_positions)
 
 class RopeSegment():
 	def __init__(self, x: int, y: int) -> None:
 		self.positionX = x
 		self.positionY = y
-	
+
 	def move_towards_point(self, x: int, y: int) -> list[tuple]:
 		deltaX, deltaY = x-self.positionX, y-self.positionY
 		if abs(deltaX) <= 1 and abs(deltaY) <= 1:
@@ -58,7 +55,7 @@ class RopeSegment():
 		out.extend(self._move_towards_point_straight(x, y))
 		self.positionX, self.positionY = out[-1]
 		return out
-	
+
 	def _move_towards_point_straight(self, x: int, y: int) -> list[tuple]:
 		deltaX = x - self.positionX
 		if deltaX == 0:
@@ -66,7 +63,7 @@ class RopeSegment():
 			return ((self.positionX, position) for position in range(self.positionY + sign, y, sign))
 		sign = 1 if deltaX > 0 else -1
 		return ((position, self.positionY) for position in range(self.positionX + sign, x, sign))
-	
+
 def calculate_grid_size(moves: tuple[tuple[int, int, int]]) -> tuple[int, int, int, int]:
 	max_position = [0, 0]
 	min_position = [0, 0]
@@ -77,9 +74,6 @@ def calculate_grid_size(moves: tuple[tuple[int, int, int]]) -> tuple[int, int, i
 		max_position[axis] = max(max_position[axis], current_position[axis])
 		min_position[axis] = min(min_position[axis], current_position[axis])
 	return *max_position, *min_position
-
-def matrix_positions2bit_field(position: tuple[tuple[int, int]], line_length: int) -> int:
-	return sum(2 ** (i + line_length * j) for i, j, in position)
 
 _input = "./2022/Day9/input.txt"
 
