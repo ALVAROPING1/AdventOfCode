f3a64b1390a6da65dc7cfa036cbad71d92b89201
ALVAROPING1 <alvaroping1@gmail.com>
2022-12-12 14:15:13 +0100

    simplified day 7 solution

--- 2022/Day7/solution.py
+++ 2022/Day7/solution.py
@@ -12,7 +12,7 @@ def part2(_input: str) -> int:
 	with open(_input, "r", encoding="UTF-8") as file:
 		lines = file.readlines()
 	priority_queue = []
-	used_size, _ = calculate_dir_size(lines, len(lines), 1, add_dir_size_queue, priority_queue)
+	used_size, _ = calculate_dir_size(lines, len(lines), 1, heapq.heappush, priority_queue)
 	required_size = 30_000_000 - (70_000_000 - used_size)
 	sorted_sizes = sorted(priority_queue)
 	return binary_search(sorted_sizes, required_size, 0, len(sorted_sizes) - 1)
@@ -30,25 +30,22 @@ class TotalSum():
 	def __init__(self, value: int = 0) -> None:
 		self.value = value
 
-def calculate_dir_size_sum(current_dir_size: int, total_sum: TotalSum) -> None:
+def calculate_dir_size_sum(total_sum: TotalSum, current_dir_size: int) -> None:
 	if current_dir_size <= 100_000:
 		total_sum.value += current_dir_size
-
-def add_dir_size_queue(current_dir_size: int, queue: list[int]) -> None:
-	heapq.heappush(queue, current_dir_size)
 
 def calculate_dir_size(lines: list[str], len_lines: int, index: int, current_dir_size_function: Callable[[int, object], None], state_object: object) -> tuple[int, int]:
 	current_dir_size = 0
 	index += 1
 	while True:
 		if index >= len_lines:
-			current_dir_size_function(current_dir_size, state_object)
+			current_dir_size_function(state_object, current_dir_size)
 			return current_dir_size, index
 		line = lines[index]
 		index += 1
 		if line[0] == "$":
 			if line[5] == ".":
-				current_dir_size_function(current_dir_size, state_object)
+				current_dir_size_function(state_object, current_dir_size)
 				return current_dir_size, index
 			dir_size, index = calculate_dir_size(lines, len_lines, index, current_dir_size_function, state_object)
 			current_dir_size += dir_size
