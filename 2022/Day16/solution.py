ce93cf7b5f311109893818e1a337e0facb715627
ALVAROPING1 <alvaroping1@gmail.com>
2023-01-01 16:36:45 +0100

    optimized day 16 solution

--- 2022/Day16/solution.py
+++ 2022/Day16/solution.py
@@ -3,99 +3,99 @@ from re import search, finditer
 from itertools import permutations, combinations
 
 def part1(_input: str) -> int:
-	graph = Graph.from_file(_input)
-	return graph.get_max_pressure("AA", 30, 12, 14)[0]
+	graph, start_id = Graph.from_file(_input)
+	return graph.get_max_pressure(start_id, 30, 12, 14)[0]
 
 def part2(_input: str) -> int:
-	graph = Graph.from_file(_input)
-	return get_max_2_partition(graph.get_max_pressure("AA", 26, 6, 7)[1])
+	graph, start_id = Graph.from_file(_input)
+	return get_max_2_partition(graph.get_max_pressure(start_id, 26, 6, 7)[1], start_id)
 
 @dataclass
 class Graph:
-	edges: dict[str, dict[str, int]]
-	vertex_values: dict[str, int]
-
-	@property
-	def vertices(self) -> tuple[str]:
-		return self.vertex_values.keys()
-
-	def get_edge_weight(self, start: str, end: str) -> int:
-		try:
-			return self.edges[start][end]
-		except KeyError:
-			return 100000
+	edges: list[list[int]]
+	vertex_values: list[int]
 
 	@classmethod
-	def from_file(cls, _input: str) -> "Graph":
-		graph = cls({}, {})
+	def from_file(cls, _input: str) -> tuple["Graph", int]:
+		graph = cls(None, [])
+		str2id = {}
+		adj_vertices_str = []
 		with open(_input, "r", encoding="UTF-8") as file:
-			for valve in file.readlines():
-				current = valve[6:8]
-				graph.vertex_values[current] = int(search(r"\d+", valve[23:25]).group())
-				graph.edges[current] = {}
-				graph.edges[current][current] = 0
-				for end in finditer(r"[A-Z][A-Z]", valve[49:]):
-					graph.edges[current][end.group()] = 1
-		graph.convert_to_distance_matrix()
-		graph.remove_stuck_valves()
-		return graph
+			for _id, valve in enumerate(file.readlines()):
+				str2id[valve[6:8]] = _id
+				graph.vertex_values.append(int(search(r"\d+", valve[23:25]).group()))
+				adj_vertices_str.append(valve[49:])
+		adj_vertices = tuple(
+			tuple(str2id[v.group()] for v in finditer(r"[A-Z][A-Z]", _valve))
+			for _valve in adj_vertices_str
+		)
+		graph.calculate_distance_matrix(adj_vertices)
+		return cls.remove_stuck_valves(graph, str2id["AA"])
 
-	def convert_to_distance_matrix(self):
-		for k, i, j in permutations(self.vertices, r=3):
+	def calculate_distance_matrix(self, adj_vertices: tuple[tuple[int]]):
+		self.edges = [[1 << 30 for _ in range(len(adj_vertices))] for _ in range(len(adj_vertices))]
+		for start, adj in enumerate(adj_vertices):
+			self.edges[start][start] = 0
+			for end in adj:
+				self.edges[start][end] = 1
+				self.edges[end][start] = 1
+		for k, i, j in permutations(range(len(adj_vertices)), r=3):
 			self.edges[i][j] = min(
-				self.get_edge_weight(i, j),
-				self.get_edge_weight(i, k) + self.get_edge_weight(k, j)
+				self.edges[i][j],
+				self.edges[i][k] + self.edges[k][j]
 			)
 
-	def remove_stuck_valves(self):
-		for vertex, value in self.vertex_values.copy().items():
-			if value != 0:
-				continue
-			for end in self.edges[vertex].copy().keys():
-				del self.edges[end][vertex]
-			if vertex != "AA":
-				del self.edges[vertex]
-				del self.vertex_values[vertex]
-				try:
-					del self.edges["AA"][vertex]
-				except KeyError:
-					continue
+	@classmethod
+	def remove_stuck_valves(cls, graph: "Graph", start_id: int) -> tuple["Graph", int]:
+		new_graph = cls([], [])
+		new_start_id = start_id
+		for start, adj_vertices in enumerate(graph.edges):
+			if graph.vertex_values[start] > 0 or start == start_id:
+				current = []
+				for end, distance in enumerate(adj_vertices):
+					if graph.vertex_values[end] > 0 or end == start_id:
+						current.append(distance)
+				new_graph.edges.append(current)
+				new_graph.vertex_values.append(graph.vertex_values[start])
+			elif start < start_id:
+				new_start_id -= 1
+		return new_graph, new_start_id
 
 	def get_max_pressure(
 		self,
-		start_vertex: str,
+		start_vertex: int,
 		start_time: int,
 		min_flow_1: int,
 		min_time_1: int,
-	) -> tuple[int, dict[frozenset[str], int]]:
+	) -> tuple[int, dict[int, int]]:
 		max_pressure = 0
 		max_pressure_dict = {}
-		visited = frozenset()
+		visited = 1 << start_vertex
 		stack = [(start_time, visited, start_vertex, max_pressure)]
 		while stack:
 			time, visited, current_vertex, current_pressure = stack.pop()
 			max_pressure = max(max_pressure, current_pressure)
 			if visited not in max_pressure_dict or max_pressure_dict[visited] < current_pressure:
 				max_pressure_dict[visited] = current_pressure
-			for adjacent in self.edges[current_vertex]:
+			for adjacent, flow in enumerate(self.vertex_values):
 				if (
-					adjacent not in visited
+					visited & 1 << adjacent == 0
 					and time > self.edges[current_vertex][adjacent]
-					and (self.vertex_values[adjacent] >= min_flow_1 or time < min_time_1)
+					and (flow >= min_flow_1 or time < min_time_1)
 				):
 					new_time = time - self.edges[current_vertex][adjacent] - 1
 					stack.append((
 						new_time,
-						visited | frozenset({adjacent}),
+						visited | 1 << adjacent,
 						adjacent,
-						current_pressure + (self.vertex_values[adjacent]) * new_time
+						current_pressure + flow * new_time
 					))
 		return max_pressure, max_pressure_dict
 
-def get_max_2_partition(_dict: dict[frozenset[str], int]) -> int:
+def get_max_2_partition(_dict: dict[int, int], start_vertex: int) -> int:
 	max_value = 0
 	for key1, key2 in combinations(_dict, r=2):
-		if key1 & key2 == set():
+		if key1 & key2 == 1 << start_vertex:
 			max_value = max(max_value, _dict[key1] + _dict[key2])
 	return max_value
 
