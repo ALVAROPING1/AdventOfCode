8404e93c0d139ea49b6bbd1274038f9b97610ac4
ALVAROPING1 <alvaroping1@gmail.com>
2023-01-24 18:51:00 +0100

    added missing newline at the end of day1 input

--- 2022/Day1/solution.py
+++ 2022/Day1/solution.py
@@ -2,11 +2,11 @@ from heapq import nlargest
 
 def part1(_input: str) -> int:
 	with open(_input, "r", encoding="UTF-8") as file:
-		return max(map(sum, (map(int, x.split("\n")) for x in file.read().split("\n\n"))))
+		return max(map(sum, (map(int, x.split("\n")) for x in file.read()[:-1].split("\n\n"))))
 
 def part2(_input: str) -> int:
 	with open(_input, "r", encoding="UTF-8") as file:
-		return sum(nlargest(3, map(sum, (map(int, x.split("\n")) for x in file.read().split("\n\n")))))
+		return sum(nlargest(3, map(sum, (map(int, x.split("\n")) for x in file.read()[:-1].split("\n\n")))))
 
 _input = "./2022/Day1/input.txt"
 
