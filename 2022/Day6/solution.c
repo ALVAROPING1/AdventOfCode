3eafe9bf96481366864d7c6be7a3833aca5bc876
ALVAROPING1 <alvaroping1@gmail.com>
2023-01-28 23:25:39 +0100

    day 6 solution in C

---
+++ 2022/Day6/solution.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int solve(const int max_size) {
+	int i = 1;
+	int window_start = 0;
+	int last_seen_index;
+	int last_seen[26] = {0};
+	char current;
+	while ((current = fgetc(stdin)) != EOF) {
+		last_seen_index = last_seen[current - 'a'];
+		if (last_seen_index > window_start) {window_start = last_seen_index;}
+		else if (i - window_start >= max_size) {return i;}
+		last_seen[current - 'a'] = i;
+		i++;
+	}
+}
+
+int part1() {
+	printf("Part 1: %d\n", solve(4));
+	return 0;
+}
+
+int part2() {
+	printf("Part 2: %d\n", solve(14));
+	return 0;
+}
+
+int main() {
+	//part1();
+	part2();
+	return 0;
+}
