6f7ff1d30c74dbcdd8dae48ac3f75c3a98574c3b
ALVAROPING1 <alvaroping1@gmail.com>
2024-12-01 13:26:23 +0100

    day 1 solution

--- aoc2024/src/day01.rs
+++ aoc2024/src/day01.rs
@@ -1,17 +1,37 @@
-use std::error::Error;
+use std::{error::Error, iter::zip};
 
 use crate::prelude::*;
 
 pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
-    Ok(Solution::default().part1(part1(input)).part2(part2(input)))
+    let (mut left, mut right) = process_lines(input);
+    Ok(Solution::default()
+        .part1(part1(&mut left, &mut right))
+        .part2(part2(&left, &right)))
+}
+
+fn process_lines(input: &str) -> (Vec<usize>, Vec<usize>) {
+    fn parse(x: &str) -> usize {
+        x.parse().expect("All values should be integers")
+    }
+
+    input
+        .split_terminator('\n')
+        .map(|line| (parse(&line[..5]), parse(&line[8..])))
+        .unzip()
 }
 
 #[must_use]
-pub fn part1(input: &str) -> u32 {
-    todo!()
+pub fn part1(left: &mut Vec<usize>, right: &mut Vec<usize>) -> usize {
+    left.sort_unstable();
+    right.sort_unstable();
+    zip(left, right).map(|(l, r)| r.abs_diff(*l)).sum()
 }
 
 #[must_use]
-pub fn part2(input: &str) -> u32 {
-    todo!()
+pub fn part2(left: &[usize], right: &[usize]) -> usize {
+    let mut counts: [u8; 100_000] = [0; 100_000];
+    for x in right {
+        counts[*x] += 1;
+    }
+    left.iter().map(|l| l * usize::from(counts[*l])).sum()
 }
