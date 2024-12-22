f12e3c6ccb10ebbfe180ea10d5bdee19078511e9
ALVAROPING1 <alvaroping1@gmail.com>
2024-12-22 15:10:18 +0100

    optimized day 22 solution

--- aoc2024/src/day22.rs
+++ aoc2024/src/day22.rs
@@ -9,11 +9,35 @@ pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
     Ok(Solution::default().part1(part1(input)).part2(part2(input)))
 }
 
-const fn next(mut state: u64) -> u64 {
-    const MODULE: u64 = 16_777_216;
-    state = ((state << 6) ^ state) % MODULE;
-    state = ((state >> 5) ^ state) % MODULE;
-    ((state << 11) ^ state) % MODULE
+struct Secret(u64);
+impl Iterator for Secret {
+    type Item = u64;
+    fn next(&mut self) -> Option<Self::Item> {
+        const MODULE: u64 = 16_777_216;
+        self.0 = (self.0 ^ (self.0 << 6)) % MODULE;
+        self.0 = (self.0 ^ (self.0 >> 5)) % MODULE;
+        self.0 = (self.0 ^ (self.0 << 11)) % MODULE;
+        Some(self.0)
+    }
+}
+
+struct DeltaPrice(u8, Secret);
+
+impl DeltaPrice {
+    const fn new(n: u64) -> Self {
+        Self((n % 10) as u8, Secret(n))
+    }
+}
+
+impl Iterator for DeltaPrice {
+    type Item = (i8, u8);
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_secret = self.1.next().expect("The iterator is infinite");
+        let prev_price = self.0;
+        self.0 = (next_secret % 10) as u8;
+        #[allow(clippy::cast_possible_wrap)]
+        Some((self.0 as i8 - prev_price as i8, self.0))
+    }
 }
 
 fn parse_input(input: &str) -> impl Iterator<Item = u64> + '_ {
@@ -25,59 +49,26 @@ fn parse_input(input: &str) -> impl Iterator<Item = u64> + '_ {
 #[must_use]
 fn part1(input: &str) -> u64 {
     parse_input(input)
-        .map(|state: u64| (0..2000).fold(state, |state, _| next(state)))
+        .map(|state: u64| Secret(state).nth(1999).expect("The iterator is infinite"))
         .sum()
 }
 
 #[must_use]
 fn part2(input: &str) -> u64 {
-    let buyers = parse_input(input)
-        // Convert initial state to hashmap of sequences of 4 changes to the price at the end of
-        // the sequence
-        .map(|mut state: u64| {
-            // Calculate initial price
-            let init = i8::try_from(state % 10).expect("The value should always fit");
-            // Create iterator of the sequence of prices
-            std::iter::repeat_with(move || {
-                // Update state
-                state = next(state);
-                // Calculate price from state
-                i8::try_from(state % 10).expect("The value should always fit")
-            })
-            // Take first N prices
-            .take(2000)
-            // Transform prices to tuples (change from the previous, current price)
-            .scan(init, |prev, price| {
-                let diff = price - *prev;
-                *prev = price;
-                Some((diff, price))
-            })
-            // Get windows of 4 consecutive tuples of (change, price)
-            .tuple_windows()
-            // Transform iterator to key-value tuples, where the key is 4 consecutive changes and
-            // the value is the last price
-            .map(|((a, _), (b, _), (c, _), (d, price))| ((a, b, c, d), price))
-            // Store the key-value pairs on a hashmap, keeping only the first pair for each key
-            .fold(HashMap::new(), |mut acc, (seq, price)| {
-                acc.entry(seq).or_insert(price);
-                acc
-            })
+    *parse_input(input)
+        .fold(HashMap::new(), |mut totals, seed| {
+            let delta_price = DeltaPrice::new(seed);
+            let sequences = delta_price
+                .take(2000)
+                .tuple_windows()
+                .map(|((a, _), (b, _), (c, _), (d, price))| ((a, b, c, d), price))
+                .unique_by(|x| x.0);
+            for (seq, price) in sequences {
+                *totals.entry(seq).or_default() += u64::from(price);
+            }
+            totals
         })
-        .collect_vec();
-    buyers
-        .iter()
-        // For each unique sequence on any buyer
-        .flat_map(|buyer| buyer.keys())
-        .unique()
-        // Calculate the total bananas for each sequence
-        .map(|seq| {
-            #[allow(clippy::cast_sign_loss)]
-            buyers
-                .iter()
-                .map(|buyer| *buyer.get(seq).unwrap_or(&0) as u64)
-                .sum()
-        })
-        // Get the maximum total bananas for any sequence
+        .values()
         .max()
         .expect("There should be at least 1 sequence")
 }
