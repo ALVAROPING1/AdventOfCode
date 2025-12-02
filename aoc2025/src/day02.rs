462af7a3519a2bd29f788188d8280dd71118d11b
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-02 15:47:04 +0100

    optimized day 2 solution

--- aoc2025/src/day02.rs
+++ aoc2025/src/day02.rs
@@ -5,10 +5,6 @@ use crate::prelude::*;
 
 type Num<'a> = (usize, &'a str);
 
-fn parse_int(x: &str) -> usize {
-    x.parse().unwrap_or(0)
-}
-
 pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
     let ranges = process_input(input);
     Ok(Solution::default()
@@ -17,28 +13,31 @@ pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
 }
 
 fn process_input(input: &str) -> Vec<(Num<'_>, Num<'_>)> {
+    let parse = |x: &str| -> usize { x.parse().expect("All values should be integers") };
     input[..input.len() - 1]
         .split(',')
         .map(|x| {
             let (s, e) = x.split_once('-').expect("There should be a `-`");
-            ((parse_int(s), s), (parse_int(e), e))
+            ((parse(s), s), (parse(e), e))
         })
         .collect()
 }
 
 #[must_use]
-fn solve<'a, F, G>(ranges: &[(Num<'a>, Num<'a>)], gen: F) -> usize
+fn solve<'a, F, G, const SET: bool>(ranges: &[(Num<'a>, Num<'a>)], gen: F) -> usize
 where
-    F: Fn(&str, &str) -> G,
+    F: Fn(&str, &str, usize, usize) -> G,
     G: Iterator<Item = usize>,
 {
     let mut total = 0;
     let mut set = HashSet::new();
     for ((start, start_str), (end, end_str)) in ranges {
-        for x in gen(start_str, end_str) {
-            if x >= *start && x <= *end && !set.contains(&x) {
+        for x in gen(start_str, end_str, *start, *end) {
+            if x >= *start && x <= *end && !(SET && set.contains(&x)) {
                 total += x;
-                set.insert(x);
+                if SET {
+                    set.insert(x);
+                }
             }
         }
     }
@@ -47,21 +46,30 @@ where
 
 #[must_use]
 fn part1(ranges: &[(Num<'_>, Num<'_>)]) -> usize {
-    solve::<_, _>(ranges, |start, end| {
-        let (len02, len12) = (start.len() / 2, end.len().div_ceil(2));
-        let (start, end) = (parse_int(&start[..len02]), parse_int(&end[..len12]));
-        (start..=end).map(|x| parse_int(&format!("{x}{x}")))
+    solve::<_, _, false>(ranges, |start, _, s, e| {
+        let chunk_size = start.len() / 2;
+        let shift = u32::try_from(start.len() - chunk_size).expect("Numbers should be small");
+        let (start, end) = (s / 10usize.pow(shift), e / 10usize.pow(shift));
+        (start..=end).filter(|&x| x != 0).map(|x| {
+            let shift = 10usize.pow(x.ilog10() + 1);
+            x * shift + x
+        })
     })
 }
 
 #[must_use]
 fn part2(ranges: &[(Num<'_>, Num<'_>)]) -> usize {
-    solve::<_, _>(ranges, |start, end| {
+    solve::<_, _, true>(ranges, |start, end, s, e| {
         (2..=end.len())
             .flat_map(|reps| {
-                let (len0, len1) = (start.len() / reps, end.len().div_ceil(reps));
-                let (start, end) = (parse_int(&start[..len0]), parse_int(&end[..len1]));
-                (start..=end).map(move |x| parse_int(&x.to_string().repeat(reps)))
+                let chunk_size = start.len() / reps;
+                let shift =
+                    u32::try_from(start.len() - chunk_size).expect("Numbers should be small");
+                let (start, end) = (s / 10usize.pow(shift), e / 10usize.pow(shift));
+                (start..=end).filter(move |&x| x != 0).map(move |x| {
+                    let shift = 10usize.pow(x.ilog10() + 1);
+                    (0..reps).fold(0, |acc, _| acc * shift + x)
+                })
             })
             .collect::<Vec<_>>()
             .into_iter()
