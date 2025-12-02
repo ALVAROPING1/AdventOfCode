4119cc9664d65889da0a92bcb2792f38d143d420
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-02 16:14:13 +0100

    refactor day 2 solution

--- aoc2025/src/day02.rs
+++ aoc2025/src/day02.rs
@@ -3,36 +3,41 @@ use std::error::Error;
 
 use crate::prelude::*;
 
-type Num<'a> = (usize, &'a str);
+type Num = (usize, usize);
 
 pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
     let ranges = process_input(input);
     Ok(Solution::default()
-        .part1(part1(&ranges))
-        .part2(part2(&ranges)))
+        .part1(solve::<false>(&ranges, |_| 2))
+        .part2(solve::<true>(&ranges, |end| end)))
 }
 
-fn process_input(input: &str) -> Vec<(Num<'_>, Num<'_>)> {
+fn process_input(input: &str) -> Vec<(Num, Num)> {
     let parse = |x: &str| -> usize { x.parse().expect("All values should be integers") };
     input[..input.len() - 1]
         .split(',')
         .map(|x| {
             let (s, e) = x.split_once('-').expect("There should be a `-`");
-            ((parse(s), s), (parse(e), e))
+            ((parse(s), s.len()), (parse(e), e.len()))
         })
         .collect()
 }
 
 #[must_use]
-fn solve<'a, F, G, const SET: bool>(ranges: &[(Num<'a>, Num<'a>)], gen: F) -> usize
-where
-    F: Fn(&str, &str, usize, usize) -> G,
-    G: Iterator<Item = usize>,
-{
+fn solve<const SET: bool>(ranges: &[(Num, Num)], reps: impl Fn(usize) -> usize) -> usize {
     let mut total = 0;
     let mut set = HashSet::new();
-    for ((start, start_str), (end, end_str)) in ranges {
-        for x in gen(start_str, end_str, *start, *end) {
+    for ((start, start_size), (end, end_size)) in ranges {
+        let candidates = (2..=reps(*end_size)).flat_map(|reps| {
+            let chunk_size = start_size / reps;
+            let shift = u32::try_from(start_size - chunk_size).expect("Numbers should be small");
+            let (start, end) = (start / 10usize.pow(shift), end / 10usize.pow(shift));
+            (start..=end).filter(|x: &usize| *x != 0).map(move |x| {
+                let shift = 10usize.pow(x.ilog10() + 1);
+                (0..reps).fold(0, |acc, _| acc * shift + x)
+            })
+        });
+        for x in candidates {
             if x >= *start && x <= *end && !(SET && set.contains(&x)) {
                 total += x;
                 if SET {
@@ -42,36 +47,4 @@ where
         }
     }
     total
-}
-
-#[must_use]
-fn part1(ranges: &[(Num<'_>, Num<'_>)]) -> usize {
-    solve::<_, _, false>(ranges, |start, _, s, e| {
-        let chunk_size = start.len() / 2;
-        let shift = u32::try_from(start.len() - chunk_size).expect("Numbers should be small");
-        let (start, end) = (s / 10usize.pow(shift), e / 10usize.pow(shift));
-        (start..=end).filter(|&x| x != 0).map(|x| {
-            let shift = 10usize.pow(x.ilog10() + 1);
-            x * shift + x
-        })
-    })
-}
-
-#[must_use]
-fn part2(ranges: &[(Num<'_>, Num<'_>)]) -> usize {
-    solve::<_, _, true>(ranges, |start, end, s, e| {
-        (2..=end.len())
-            .flat_map(|reps| {
-                let chunk_size = start.len() / reps;
-                let shift =
-                    u32::try_from(start.len() - chunk_size).expect("Numbers should be small");
-                let (start, end) = (s / 10usize.pow(shift), e / 10usize.pow(shift));
-                (start..=end).filter(move |&x| x != 0).map(move |x| {
-                    let shift = 10usize.pow(x.ilog10() + 1);
-                    (0..reps).fold(0, |acc, _| acc * shift + x)
-                })
-            })
-            .collect::<Vec<_>>()
-            .into_iter()
-    })
 }
