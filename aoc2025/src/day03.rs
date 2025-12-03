cddcd923ad3c61dcdda674346e5471d491919fe1
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-03 19:46:14 +0100

    refactor day 3 solution

--- aoc2025/src/day03.rs
+++ aoc2025/src/day03.rs
@@ -5,53 +5,31 @@ use crate::prelude::*;
 pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
     let banks = process_input(input);
     Ok(Solution::default()
-        .part1(part1(banks.clone()))
-        .part2(part2(banks)))
+        .part1(solve(&banks, 2))
+        .part2(solve(&banks, 12)))
 }
 
-fn process_input(input: &str) -> impl Iterator<Item = &[u8]> + Clone {
-    input.split_terminator('\n').map(str::as_bytes)
-}
-
-#[must_use]
-fn part1<'a>(banks: impl Iterator<Item = &'a [u8]>) -> usize {
-    let mut total: usize = 0;
-    for bank in banks {
-        let mut maxs = std::iter::repeat_n(0, bank.len()).collect::<Vec<_>>();
-        let max_pos = bank.len() - 1;
-        let mut max = 0;
-        maxs[max_pos] = (bank[max_pos] - b'0') as usize;
-        for (i, val) in bank.iter().enumerate().rev().skip(1) {
-            let val = (*val - b'0') as usize;
-            max = max.max(val * 10 + maxs[i + 1]);
-            maxs[i] = maxs[i + 1].max(val);
-        }
-        total += max;
-    }
-    total
+fn process_input(input: &str) -> Vec<&[u8]> {
+    input.split_terminator('\n').map(str::as_bytes).collect()
 }
 
 #[must_use]
-fn part2<'a>(banks: impl Iterator<Item = &'a [u8]>) -> usize {
-    const DIGITS: usize = 12;
+fn solve(banks: &[&[u8]], digits: usize) -> usize {
     let mut total: usize = 0;
-    let mut num = Vec::<u8>::with_capacity(DIGITS);
-    let mut pos = Vec::<usize>::with_capacity(DIGITS);
+    let mut num: Vec<u8> = std::iter::repeat_n(0, digits).collect();
+    let mut pos = Vec::<usize>::with_capacity(digits);
     for bank in banks {
-        num.clear();
         pos.clear();
-        num.extend(bank[..DIGITS].iter());
-        pos.extend(0..DIGITS);
+        num.copy_from_slice(&bank[..digits]);
+        pos.extend(0..digits);
         for (i, new) in bank.iter().enumerate().skip(1) {
-            let right = DIGITS.min(i + 1);
-            let left = DIGITS.saturating_sub(bank.len() - i);
+            let right = digits.min(i + 1);
+            let left = digits.saturating_sub(bank.len() - i);
             for j in left..right {
-                let curr = num[j];
-                let curr_pos = pos[j];
-                if curr_pos >= i {
+                if pos[j] >= i {
                     break;
-                } else if *new > curr {
-                    let new_digits = i..i + DIGITS - j;
+                } else if *new > num[j] {
+                    let new_digits = i..i + digits - j;
                     num[j..].copy_from_slice(&bank[new_digits.clone()]);
                     pos.truncate(j);
                     pos.extend(new_digits);
