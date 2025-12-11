1e132e4132436e3977d9632926885e5c450a0a4b
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-11 14:02:15 +0100

    refactor day 11 solution

--- aoc2025/src/day11.rs
+++ aoc2025/src/day11.rs
@@ -1,4 +1,5 @@
-use std::{collections::HashMap, error::Error};
+use std::collections::HashMap;
+use std::error::Error;
 
 use crate::prelude::*;
 
@@ -16,22 +17,19 @@ struct Graph<'a> {
 
 #[must_use]
 fn parse_input(input: &str) -> Graph<'_> {
-    let mut total = 0;
+    let mut total = 0..;
     let mut ids = HashMap::new();
+    macro_rules! get_id {
+        ($x:ident) => {
+            *ids.entry($x).or_insert_with(|| total.next().unwrap())
+        };
+    }
     let mut connections: Vec<Vec<usize>> = Vec::new();
     for line in input.split_terminator('\n') {
         let (origin, targets) = line.split_once(':').expect("There should be a `:`");
-        let o = *ids.entry(origin).or_insert_with(|| {
-            let id = total;
-            total += 1;
-            id
-        });
+        let o = get_id!(origin);
         for target in targets[1..].split_whitespace() {
-            let t = *ids.entry(target).or_insert_with(|| {
-                let id = total;
-                total += 1;
-                id
-            });
+            let t = get_id!(target);
             if t >= connections.len() {
                 connections.resize_with(t + 1, Default::default);
             }
@@ -41,71 +39,60 @@ fn parse_input(input: &str) -> Graph<'_> {
     Graph { ids, connections }
 }
 
-fn solve(g: &Graph, paths: &mut [Option<usize>], start: usize, node: usize) -> usize {
-    let count: usize = g.connections[node]
+fn solve<F, const N: usize>(
+    g: &Graph,
+    paths: &mut [Option<[usize; N]>],
+    merge: F,
+    start: usize,
+    node: usize,
+) -> [usize; N]
+where
+    F: Fn(&mut [usize; N], usize) + Copy,
+{
+    if node == start {
+        let mut res = [0; N];
+        res[0] = 1;
+        return res;
+    }
+    g.connections[node]
         .iter()
         .map(|&o| {
             paths[o].map_or_else(
                 || {
-                    let res = solve(g, paths, start, o);
+                    let mut res = solve(g, paths, merge, start, o);
+                    merge(&mut res, o);
                     paths[o] = Some(res);
                     res
                 },
                 |x| x,
             )
         })
-        .sum();
-    count + usize::from(node == start)
-}
-
-#[must_use]
-fn part1(g: &Graph) -> usize {
-    solve(g, &mut vec![None; g.ids.len()], g.ids["you"], g.ids["out"])
-}
-
-fn solve2(
-    g: &Graph,
-    paths: &mut (&mut [Option<[usize; 4]>], [usize; 2]),
-    start: usize,
-    node: usize,
-) -> [usize; 4] {
-    let count: [usize; 4] = g.connections[node]
-        .iter()
-        .map(|&o| {
-            paths.0[o].map_or_else(
-                || {
-                    let mut res = solve2(g, paths, start, o);
-                    if o == paths.1[0] {
-                        res[1] = res[0];
-                        res[3] = res[2];
-                    } else if o == paths.1[1] {
-                        res[2] = res[0];
-                        res[3] = res[1];
-                    }
-                    paths.0[o] = Some(res);
-                    res
-                },
-                |x| x,
-            )
-        })
-        .reduce(|mut acc, x| {
+        .fold([0; N], |mut acc, x| {
             for (acc, x) in std::iter::zip(&mut acc, x) {
                 *acc += x;
             }
             acc
         })
-        .unwrap_or([0, 0, 0, 0]);
-    [
-        count[0] + usize::from(node == start),
-        count[1],
-        count[2],
-        count[3],
-    ]
+}
+
+#[must_use]
+fn part1(g: &Graph) -> usize {
+    let mut paths = vec![None; g.ids.len()];
+    solve::<_, 1>(g, &mut paths, |_, _| {}, g.ids["you"], g.ids["out"])[0]
 }
 
 #[must_use]
 fn part2(g: &Graph) -> usize {
-    let mut cache = vec![None; g.ids.len()];
-    let mut paths = (&mut cache[..], [g.ids["dac"], g.ids["fft"]]);
-    solve2(g, &mut paths, g.ids["svr"], g.ids["out"])[3]
+    let mut paths = vec![None; g.ids.len()];
+    let midpoints = [g.ids["dac"], g.ids["fft"]];
+    let merge = |paths: &mut [_; _], o| {
+        if o == midpoints[0] {
+            paths[1] = paths[0];
+            paths[3] = paths[2];
+        } else if o == midpoints[1] {
+            paths[2] = paths[0];
+            paths[3] = paths[1];
+        }
+    };
+    solve::<_, 4>(g, &mut paths, merge, g.ids["svr"], g.ids["out"])[3]
 }
