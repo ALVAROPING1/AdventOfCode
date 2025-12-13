6bfd7e06b4f58cd02e77abcd59d96ee8fbee6a52
ALVAROPING1 <alvaroping1@gmail.com>
2025-12-13 17:29:36 +0100

    simplified day 12 solution

--- aoc2025/src/day12.rs
+++ aoc2025/src/day12.rs
@@ -3,33 +3,25 @@ use std::error::Error;
 use crate::prelude::*;
 
 pub fn run(input: &str) -> Result<Solution, Box<dyn Error>> {
-    let (shapes, regions) = parse_input(input);
     Ok(Solution::default()
-        .part1(part1(&shapes, regions))
+        .part1(part1(input))
         .part2("Nothing to solve".to_string()))
 }
 
-fn parse_input(input: &str) -> (Vec<usize>, &str) {
-    let (shapes, regions) = input
-        .rsplit_once("\n\n")
-        .expect("There should be 2 sections");
-    let shapes = shapes
-        .split("\n\n")
-        .map(|shape| shape.chars().filter(|c| *c == '#').count())
-        .collect();
-    (shapes, regions)
-}
-
 #[must_use]
-fn part1(shapes: &[usize], regions: &str) -> usize {
+fn part1(input: &str) -> usize {
     let parse = |x: &str| -> usize { x.parse().expect("All values should be integers") };
-    let regions = regions.split_terminator('\n').map(|line| {
-        let (area, counts) = line.split_once(": ").expect("There should be 2 sections");
-        let (x, y) = area.split_once('x').expect("There should be 2 sections");
-        let area = parse(x) * parse(y);
-        let counts = counts.split_whitespace().map(parse);
-        let total: usize = counts.zip(shapes).map(|(count, area)| count * area).sum();
-        (area, total)
-    });
-    regions.filter(|(area, total)| total < area).count()
+    input
+        .rsplit_once("\n\n")
+        .expect("There should be 2 sections")
+        .1
+        .split_terminator('\n')
+        .filter(|line| {
+            // In the input, the presents either fit trivially with a lot of leftover space, or
+            // they trivially would require at least more area than there is on the input
+            let (area, counts) = line.split_once(": ").expect("There should be 2 sections");
+            let (x, y) = area.split_once('x').expect("There should be 2 sections");
+            counts.split_whitespace().map(parse).sum::<usize>() * 9 <= parse(x) * parse(y)
+        })
+        .count()
 }
